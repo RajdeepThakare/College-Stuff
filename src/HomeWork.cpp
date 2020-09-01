0feac66dd651b1e3c613ca437b80932b857a3250
RajdeepThakare <rajdeep.ravithakare@gmail.com>
2020-09-01 18:30:24 +0530

    First Commit

---
+++ src/HomeWork.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : HomeWork.cpp
+// Author      : Rajdeep Thakare 19Q91A0548
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
