594b1e7a283248e7ba296692a135b4acf7971588
RajdeepThakare <rajdeep.ravithakare@gmail.com>
2020-09-02 07:45:34 +0530

    update 1

--- src/HomeWork.cpp
+++ src/HomeWork.cpp
@@ -1,13 +1,43 @@
 //============================================================================
 // Name        : HomeWork.cpp
 // Author      : Rajdeep Thakare 19Q91A0548
-// Description : Hello World in C++, Ansi-style
+// Description : Program to accept name of an item along with rate and quantity
 //============================================================================
 
 #include <iostream>
-using namespace std;
+
+using std::cout;
+using std::cin;
+
+char name[10];
+int rate = 10;
+float discount = 0.20;
+int total;
+int quantity;
+double discountedPrice;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	cout << "==========================\n";
+	cout << "Welcome to Stationary Shop\n";
+	cout << "==========================\n\n";
+
+
+	cout << "Type the name of an item you'd like to buy : ";
+	cin >> name;
+	cout << "How many " << name << "'s" << " would you like to buy : ";
+	cin >> quantity;
+
+	total = quantity * rate;
+
+	cout << "Item : " << name << "\n";
+	cout << "Quantity : " << quantity << "\n";
+	cout << "Total : " << total << "\n";
+
+
+	discountedPrice = total - discount * total;
+
+	cout << "Discounted price is : " << discountedPrice;
+
 	return 0;
 }
